1a67fe8a134f79546f47f172d4431a20adeadc45
Aleksei Mironov <aleksei.mironov@aciworldwide.com>
2017-06-06 21:50:24 +0300

    moved functional to S3Resolver

--- src/main/scala/io/hydrosphere/mist/jobs/resolvers/S3Resolver.scala
+++ src/main/scala/io/hydrosphere/mist/jobs/resolvers/S3Resolver.scala
@@ -2,49 +2,46 @@ package io.hydrosphere.mist.jobs.resolvers
 
 import java.io.File
 import java.net.URI
+import java.nio.file.Paths
 
+import com.amazonaws.regions.{Region, Regions}
 import com.amazonaws.services.s3.model.GetObjectRequest
-import com.amazonaws.services.s3.{AmazonS3Client, AmazonS3ClientBuilder}
-import io.hydrosphere.mist.jobs.JobFile
-import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, Path}
+import com.amazonaws.services.s3.{AmazonS3ClientBuilder, AmazonS3URI}
 
 
 class S3Resolver(
     path: String,
-    targetDir: String = "/tmp",
-    bucket: String = "",
-    key: String = ""
+    targetDir: String = "/tmp"
   ) extends JobResolver {
 
-  private val uri = new URI(path)
+  private val amazonS3URI = new AmazonS3URI(path)
 
-
-  private val hdfsAddress = s"${uri.getScheme}://${uri.getHost}:${uri.getPort}"
-
-  private lazy val fileSystem = {
-    FileSystem.get(new URI(hdfsAddress), new Configuration())
-  }
-
+  private val s3Client = AmazonS3ClientBuilder.standard()
+    .withRegion(Regions.DEFAULT_REGION)
+    .withForceGlobalBucketAccessEnabled(true)
+    .build()
 
 
   override def exists: Boolean = {
-    //fileSystem.exists(new Path(uri))
-    return true
+    s3Client.doesObjectExist(amazonS3URI.getBucket, amazonS3URI.getKey)
   }
 
   override def resolve(): File = {
+    //TODO what to do if file exists already?
     if (!exists) {
-      throw new JobFile.NotFoundException(s"file $path not found")
+      val regionStr  = amazonS3URI.getRegion
+      val bucket = amazonS3URI.getBucket
+      val key = amazonS3URI.getKey
+      var regions = Regions.DEFAULT_REGION
+      //TODO Dunno
+      if (regionStr != null) {
+        regions = Regions.fromName(regionStr)
+        s3Client.setRegion(Region.getRegion(regions))
+      }
+      //TODO thinking about name of jars
+      val localPath = Paths.get(targetDir, "%s_%s_%s.jar".format(regions, bucket, key))
+      val s3Object = s3Client.getObject(new GetObjectRequest(bucket, key), localPath.toFile)
+      new File(localPath.toString)
     }
-    val remotePath = new Path(path)
-    val checkSum = fileSystem.getFileChecksum(remotePath)
-
-    val localPath = new Path(s"$targetDir/${checkSum.toString}.jar")
-    if (!new File(localPath.toString).exists()) {
-      fileSystem.copyToLocalFile(false, remotePath, localPath, true)
-    }
-
-    new File(localPath.toString)
   }
 }
--- src/main/scala/io/hydrosphere/mist/jobs/resolvers/SomeObjectTestUrl.scala
+++ src/main/scala/io/hydrosphere/mist/jobs/resolvers/SomeObjectTestUrl.scala
@@ -1,5 +1,7 @@
 package io.hydrosphere.mist.jobs.resolvers
 
+import java.nio.file.Paths
+
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.{AmazonS3ClientBuilder, AmazonS3URI}
 import com.amazonaws.services.s3.model.GetObjectRequest
@@ -10,27 +12,11 @@ import com.amazonaws.services.s3.model.GetObjectRequest
 object SomeObjectTestUrl {
 
   val urlStr = "https://s3.amazonaws.com/nutrition001/006.html"
+  val targetDir: String = "/tmp"
 
   def main(args: Array[String]): Unit = {
-    val amazonS3URI = new AmazonS3URI(urlStr)
-    val regionStr  = amazonS3URI.getRegion
-    val bucket = amazonS3URI.getBucket
-    val key = amazonS3URI.getKey
-
-    var region = Regions.DEFAULT_REGION
-
-    //TODO Dunno
-    if (regionStr != null) {
-      region = Regions.fromName(regionStr)
-    }
-
-    val s3Client = AmazonS3ClientBuilder.standard()
-                    .withRegion(region)
-                    .withForceGlobalBucketAccessEnabled(true)
-                    .build()
-
-    val s3Object = s3Client.getObject(new GetObjectRequest(bucket, key))
-    println("Content-Type: " + s3Object.getObjectMetadata.getContentType)
+    val resolver = new S3Resolver(urlStr)
+    resolver.resolve()
   }
 
 }
