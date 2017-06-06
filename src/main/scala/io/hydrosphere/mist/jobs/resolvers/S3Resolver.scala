5030b514042ab456650384458e17c396ed815086
Aleksei <waxandmat@gmail.com>
2017-06-07 01:13:32 +0300

    added tests

--- src/main/scala/io/hydrosphere/mist/jobs/resolvers/S3Resolver.scala
+++ src/main/scala/io/hydrosphere/mist/jobs/resolvers/S3Resolver.scala
@@ -3,7 +3,8 @@ package io.hydrosphere.mist.jobs.resolvers
 import java.io.File
 import java.nio.file.Paths
 
-import com.amazonaws.auth.{AWSCredentialsProvider, DefaultAWSCredentialsProviderChain}
+import com.amazonaws.AmazonClientException
+import com.amazonaws.auth.{AWSCredentials, AWSCredentialsProvider, DefaultAWSCredentialsProviderChain}
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.model.GetObjectRequest
 import com.amazonaws.services.s3.{AmazonS3ClientBuilder, AmazonS3URI}
@@ -11,10 +12,10 @@ import io.hydrosphere.mist.jobs.JobFile
 
 
 case class S3Resolver(
-                  path: String,
-                  targetDir: String = "/tmp",
-                  credentials: AWSCredentialsProvider = new DefaultAWSCredentialsProviderChain()
-                ) extends JobResolver {
+                       path: String,
+                       targetDir: String = "/tmp",
+                       credentials: AWSCredentialsProvider = new S3CredentialsProviderChain()
+                     ) extends JobResolver {
 
   private val amazonS3URI = new AmazonS3URI(path)
 
@@ -26,7 +27,7 @@ case class S3Resolver(
 
   private def isEmpty(x: String) = Option(x).forall(_.isEmpty)
 
-  private def matchRegion (in: String): Regions = in match  {
+  private def matchRegion(in: String): Regions = in match {
     case in if isEmpty(in) => Regions.DEFAULT_REGION
     case in => Regions.fromName(in)
   }
@@ -47,5 +48,24 @@ case class S3Resolver(
     //TODO what to do if file exists already? now it overwrites file
     val s3Object = s3Client.getObject(new GetObjectRequest(bucket, key), localPath.toFile)
     new File(localPath.toString)
+  }
+}
+
+/**
+  *  it hack for provide credentials by default if it is not.
+  *  It is copy from {@link com.amazonaws.services.s3.S3CredentialsProviderChain}
+  *  it is created when we use {@link com.amazonaws.services.s3.AmazonS3ClientBuilder}
+  *  without using {@link com.amazonaws.services.s3.AmazonS3ClientBuilder#withCredentials} method
+  *  but we have to use this method if we want run it without any credentials some times
+  */
+class S3CredentialsProviderChain extends DefaultAWSCredentialsProviderChain {
+
+  override def getCredentials(): AWSCredentials = {
+    try {
+      super.getCredentials()
+    } catch {
+      case ace: AmazonClientException => {/*NOP*/}
+    }
+    null
   }
 }
--- src/test/scala/io/hydrosphere/mist/jobs/resolvers/S3ResolverTest.scala
+++ src/test/scala/io/hydrosphere/mist/jobs/resolvers/S3ResolverTest.scala
@@ -1,9 +1,41 @@
 package io.hydrosphere.mist.jobs.resolvers
 
+import java.nio.file.{Files, Paths}
+
+import io.hydrosphere.mist.jobs.JobFile
 import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
 
-class S3ResolverTest extends FunSuite with Matchers with BeforeAndAfterAll {
+class S3ResolverTest extends FunSuite with Matchers {
 
+  val existUrl = "https://nutrition001.s3.amazonaws.com/006.html"
+  val nonExistUrl = "https://someBucket123.s3.amazonaws.com/someKey123"
+  val incorectUrl =  "http://abracadabra.com/"
 
+  test("should throw exception if incorrect url") {
+    an [IllegalArgumentException] should be thrownBy new S3Resolver(incorectUrl)
+  }
+
+  test("exist should return false if this object doesnt exist") {
+    val resolver = new S3Resolver(nonExistUrl)
+    resolver.exists shouldBe false
+  }
+
+  test("exist should return true if this object exist") {
+    val resolver = new S3Resolver(existUrl)
+    resolver.exists shouldBe true
+  }
+
+  test("resolve should throw exception if this object doesnt exist") {
+    val resolver = new S3Resolver(nonExistUrl)
+    an [JobFile.NotFoundException] should be thrownBy resolver.resolve()
+  }
+
+  test("resolve should copy file from S3") {
+    val resolver = new S3Resolver(existUrl, "/tmp")
+    val file = resolver.resolve()
+    file.getPath.endsWith(".jar") shouldBe true
+    val bytes = Files.readAllBytes(Paths.get(file.getPath))
+    new String(bytes) shouldBe "JAR CONTENT"
+  }
 
 }
